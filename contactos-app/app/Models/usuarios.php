5dbb1b752ac9c0b9bec4c2266edbbe3a40d3b4ae
Carlos Ospino <futbolalucard@gmail.com>
2025-08-25 18:00:11 -0500

    avance

--- contactos-app/app/Models/usuarios.php
+++ contactos-app/app/Models/usuarios.php
@@ -1,5 +1,26 @@
 <?php
 
 class Users{
-    
+    private $db;
+    private $id;
+    private $nombre;
+    private $email;
+    private $clave;
+    private $fecha;
+
+    public function __construct($db){
+        $this->db=$db;
+    }
+
+    public function iniciarSesion($email, $clave){
+        $query="SELECT * FROM usuarios WHERE email = ? AND clave = ?";
+        $stmt=$this->db->prepare($query);
+        $stmt->execute([$email, $clave]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
+
+require '../../config/database.php';
+$cx=new Conexion();
+$persona=new Users($cx->conectar());
+print_r($persona->iniciarSesion('juan@example.com', '1234'));
--- contactos-app/config/database.php
+++ contactos-app/config/database.php
@@ -2,7 +2,8 @@
 class Conexion{
     public static function conectar(){
         try{
-            $base=new PDO('mysql:host=localhost; dbname=contactos-app', 'root', '');
+            $base=new PDO('mysql:host=localhost; dbname=contactos_app', 'root', '');
+            return $base;
         }
         catch(Exception $e){
             die("Error: " . $e->getMessage());
