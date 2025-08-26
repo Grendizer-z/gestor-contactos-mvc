a2ec0451a1bad82d7d1f4b231357bf7ee8547498
Carlos Ospino <futbolalucard@gmail.com>
2025-08-26 17:44:52 -0500

    se añade el modelo de contactos y usuarios

---
+++ contactos-app/app/Models/contactos.php
@@ -0,0 +1,39 @@
+<?php
+
+class Contactos{
+    private $db;
+    private $id;
+    private $nombre;
+    private $email;
+    private $clave;
+    private $fecha;
+
+    public function __construct($db){
+        $this->db=$db;
+    }
+
+    public function iniciarSesion($email, $clave){
+        $query="SELECT * FROM usuarios WHERE email = ? AND clave = ?";
+        $stmt=$this->db->prepare($query);
+        $stmt->execute([$email, $clave]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function registrar($nombre, $email, $clave, $fecha){
+        $query="INSERT INTO usuarios (nombre, email, clave, fecha_creacion) VALUES (?, ?, ?, ?)";
+        $stmt=$this->db->prepare($query);
+        return $stmt->execute([$nombre, $email, $clave, $fecha]);
+    }
+
+    public function getID($id){
+        $query="SELECT * FROM usuarios WHERE id = ?";
+        $stmt=$this->db->prepare($query);
+        $stmt->execute([$id]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+require '../../config/database.php';
+$cx=new Conexion();
+$persona=new Users($cx->conectar());
+$persona->registrar('Jorge', 'jorge@example.com', 'jorge', '2025-08-26');
--- contactos-app/app/Models/usuarios.php
+++ contactos-app/app/Models/usuarios.php
@@ -18,9 +18,17 @@ class Users{
         $stmt->execute([$email, $clave]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-}
 
-require '../../config/database.php';
-$cx=new Conexion();
-$persona=new Users($cx->conectar());
-print_r($persona->iniciarSesion('juan@example.com', '1234'));
+    public function registrar($nombre, $email, $clave, $fecha){
+        $query="INSERT INTO usuarios (nombre, email, clave, fecha_creacion) VALUES (?, ?, ?, ?)";
+        $stmt=$this->db->prepare($query);
+        return $stmt->execute([$nombre, $email, $clave, $fecha]);
+    }
+
+    public function getID($id){
+        $query="SELECT * FROM usuarios WHERE id = ?";
+        $stmt=$this->db->prepare($query);
+        $stmt->execute([$id]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
